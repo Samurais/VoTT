529052f8c935d4522a42787addb50b08d0236260
Phil Cherner <pcherner@mit.edu>
2019-02-25 15:18:22 -0500

    Fix copyRect bug

--- src/registerToolbar.ts
+++ src/registerToolbar.ts
@@ -90,6 +90,14 @@ export default function registerToolbar() {
     });
 
     ToolbarItemFactory.register({
+        name: ToolbarItemName.CopyRectangle,
+        tooltip: "Copy Rectangle",
+        icon: "far fa-clone",
+        group: ToolbarItemGroup.Canvas,
+        type: ToolbarItemType.State,
+    });
+
+    ToolbarItemFactory.register({
         name: ToolbarItemName.PreviousAsset,
         tooltip: "Previous Asset",
         icon: "fas fa-arrow-circle-up",
